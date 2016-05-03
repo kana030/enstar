b585ec7bba995c76491ec5051556869a478fd5ef
kana030 <kana.o030o@gmail.com>
2016-05-04 00:09:07 +0900

    add 2hours timer

--- app/src/main/java/jp/xxxxxxxx/kanalab/enstar/MainActivity.java
+++ app/src/main/java/jp/xxxxxxxx/kanalab/enstar/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.xxxxxxxx.kanalab.enstar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+    private Switch startSwitch;
+    private TextView timerView;
+    final MyCountDownTimer cdt = new MyCountDownTimer(7200000, 1000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +30,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        timerView = (TextView) findViewById(R.id.textView2);
+        startSwitch= (Switch) findViewById(R.id.switch1);
+        startSwitch.setOnCheckedChangeListener(this);
+
+    }
+
+    //トグルスイッチの状態
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (isChecked == true) {
+            //タイマーの起動
+             cdt.start();
+
+        } else {
+            //タイマーの停止
+            cdt.cancel();
+            timerView.setText("2:00:00");
+
+        }
+    }
+
+    public class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+
+        }
+
+        @Override
+        public void onFinish() {
+            // カウントダウン完了後に呼ばれる
+            timerView.setText("0:00:00");
+            Toast.makeText(getApplicationContext(), "LPがたまりました", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // インターバル(countDownInterval)毎に呼ばれる
+            timerView.setText(Long.toString(millisUntilFinished/1000/60/60) + ":" +Long.toString(millisUntilFinished/1000/60%60) + ":" + Long.toString(millisUntilFinished/1000%60));
+        }
     }
 
     @Override
